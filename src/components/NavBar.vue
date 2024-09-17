48c81ca972b220182c308a4a98ef14a440adaba8
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-17 11:45:37 +0200

    añadida estructura

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <main>
+    <div id="containerNavBar"></div>
+  </main>
+</template>
+<style scoped>
+#containerNavBar {
+  width: 100%;
+  height: 100px;
+  background-color: black;
+}
+</style>
