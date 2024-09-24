ba5f41c6ba360176b533061fc6a450cee54a038d
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-24 15:08:40 +0200

    add fixes in slider

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -2,54 +2,16 @@
 
 <template>
   <main>
-    <div
-      id="carouselPrincipal"
-      class="carousel slide"
-      data-bs-ride="carousel"
-      data-bs-interval="3000"
-    >
+    <div id="carouselPrincipal" class="carousel-static">
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img
-            src="../assets/img/slider/pizza2.png"
+            src="../assets/img/slider/pizzaSlider.png"
             class="imgSlider"
-            alt="..."
-          />
-          <div class="carousel-caption">
-            <h2>2x1 EN NAPOLITANAS</h2>
-          </div>
-        </div>
-        <div class="carousel-item">
-          <img
-            src="../assets/img/slider/pizza2.png"
-            class="imgSlider"
-            alt="..."
-          />
-        </div>
-        <div class="carousel-item">
-          <img
-            src="../assets/img/slider/pizza2.png"
-            class="imgSlider"
-            alt="..."
+            alt="Imagen de pizza"
           />
         </div>
       </div>
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselPrincipal"
-        data-bs-slide="prev"
-      >
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselPrincipal"
-        data-bs-slide="next"
-      >
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
   </main>
 </template>
@@ -57,47 +19,50 @@
 <style scoped>
 #carouselPrincipal {
   width: 100%;
-  height: 500px;
+  height: 100%;
   text-align: center;
-  margin-top: 30px;
+}
+
+main {
+  width: 100%;
+  height: 350px;
 }
 
 .imgSlider {
-  width: 50%;
-  height: 500px;
-  opacity: 0.3;
+  width: 70%;
+  height: 350px;
 }
+
 h2 {
   font-size: 200px;
   color: white;
   text-shadow: 4px 4px rgb(182, 124, 1);
   z-index: 20;
 }
-.carousel-caption {
-  height: 500px;
 
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 @media (min-width: 481px) and (max-width: 1024px) {
   h2 {
     font-size: 100px;
   }
+  .imgSlider {
+    width: 90%;
+  }
 }
+
 @media (max-width: 480px) {
+  main {
+    width: 100%;
+    height: 150px;
+  }
+
   #carouselPrincipal {
     width: 100%;
-    height: 250px;
+    height: 150px;
   }
-  .carousel-caption {
-    height: 250px;
-    text-align: center;
-  }
+
   .imgSlider {
-    width: 50%;
-    height: 250px;
+    width: 90%;
+    height: 150px;
   }
   h2 {
     text-align: center;
