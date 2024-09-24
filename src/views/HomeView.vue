f74efbbfaf9e799b71edf9caad6d5e4fe92f7d1d
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-24 12:08:47 +0200

    add component pizzas with responsive mobile and tablet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,12 +3,14 @@ import Descripcion from "@/components/Descripcion.vue";
 import Slider from "@/components/Slider.vue";
 import Postres from "@/components/Postres.vue";
 import Bebidas from "@/components/Bebidas.vue";
+import Pizzas from "@/components/Pizzas.vue";
 </script>
 
 <template>
   <main>
     <Descripcion></Descripcion>
     <Slider></Slider>
+    <Pizzas></Pizzas>
     <Postres></Postres>
     <Bebidas></Bebidas>
   </main>
