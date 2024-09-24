e28ce8e7727d16449395c58f50f090c7a277605a
Oscarss15 <117840754+Oscarss15@users.noreply.github.com>
2024-09-24 09:10:31 +0200

    add postres with responsive in mobile and tablet

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Descripcion from "@/components/Descripcion.vue";
 import Slider from "@/components/Slider.vue";
+import Postres from "@/components/Postres.vue";
 </script>
 
 <template>
   <main>
     <Descripcion></Descripcion>
     <Slider></Slider>
+    <Postres></Postres>
   </main>
 </template>
 
