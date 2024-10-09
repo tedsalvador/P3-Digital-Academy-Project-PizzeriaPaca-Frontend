243e39f7a9af6fa9a0e7d5109e6f963288995cad
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-10-09 10:45:32 +0200

    created Order Repository

---
+++ src/core/order/OrderRepository.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export default class OrderRepository {
+    constructor() {
+        this.baseUrl = import.meta.env.VITE_API_ENDPOINT
+    }
+
+    async createOrder(order) {
+        try {
+            const response = await axios.post(`${this.baseUrl}/order`, order, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                withCredentials: true
+            });
+            return response.data
+        } catch (error) {
+            throw error
+        }
+    }
+}
