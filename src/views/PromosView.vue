4614ab26f8ed1608a5a3e129d0294f2e08b79df2
Nacho Rodríguez Martín <nachorm9@hotmail.com>
2024-10-09 12:45:23 +0200

    fix duplicated navbar

--- src/views/PromosView.vue
+++ src/views/PromosView.vue
@@ -1,11 +1,10 @@
 <script setup>
-import Titulo from "@/components/Titulo.vue";
 import TituloLogueado from "@/components/TituloLogueado.vue";
 import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <Titulo></Titulo>
+ 
   <TituloLogueado></TituloLogueado>
   <NavBar></NavBar>
   <main>promos</main>
