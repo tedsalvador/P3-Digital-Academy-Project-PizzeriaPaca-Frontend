f4ff162f542ae2e2272eb1b430865c95059a76c5
tedsalvador <tedpaul@hotmail.com>
2024-10-14 00:56:28 +0200

    update 14_10_2024 register orders and order_details from shopping cart

--- src/core/order/OrderService.js
+++ src/core/order/OrderService.js
@@ -1,4 +1,5 @@
 import OrderRepository from './OrderRepository';
+import { orderDto } from './OrderDto';
 
 export default class OrderService {
     
@@ -6,7 +7,23 @@ export default class OrderService {
         this.repo = new OrderRepository()
     }
 
-    async createOrder(order) {
-        return await this.repo.createOrder(order)
+    async createOrder(orderDto) {
+        // Asegúrate de que orderDto tenga la propiedad products
+        if (!orderDto.products || !Array.isArray(orderDto.products)) {
+            console.error("El orderDto debe contener un array de productos.");
+            throw new Error("Invalid orderDto: products are missing or not an array");
+        }
+
+        console.log("Datos de la orderService.js:", JSON.stringify(orderDto, null, 2));
+            try {
+                const result = await this.repo.createOrder(orderDto); // Retorna directamente la respuesta
+                console.log("Respuesta del backend:", result); // Usa result directamente sin 'result.data' aquí
+                return result; // Retorna la respuesta completa del backend
+            } catch (error) {
+                console.error('Error al crear la orden:', error);
+                throw error; // Lanza el error para manejarlo en la capa superior
+            }
+
+
     }
 }
